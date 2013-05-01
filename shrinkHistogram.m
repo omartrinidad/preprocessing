1167da1378e2bfae681218d92336b5fe331e1597
Omar Trinidad Gutiérrez Méndez <omar.vpa@gmail.com>
2013-05-01 12:22:01 -0500

    Added shrink histogram implementation

--- shrinkHistogram.m
+++ shrinkHistogram.m
@@ -1,18 +1,23 @@
-% manual histogram
-function [bins] = shrinkHistogram(image, grayrange)
-    image = imadjust(image, [0, 1.0], [0.5, 0.8]);
-    
-    xy = size(image);
-    bins = zeros(1, 2^grayrange);
+% Shrink histogram to compress the image
 
-    for x = 1:1:xy(1)
-        for y = 1:1:xy(2)
-            value = image(x, y);
-            disp(x), disp(y);
-            value = value + 1; %avoid zero index
-            bins(value) = bins(value) + 1;
+function [newData usedGraylevels] = shrinkHistogram(image, grayrange)
+    %image = imadjust(image, [0, 1.0], [0.5, 0.8]);
+    grays = 2^grayrange;
+    data = imhist(image, grays); 
+    usedGraylevels = 0;
+    for i=1:1:grays
+        if data(i) ~= 0
+            usedGraylevels = usedGraylevels + 1;
         end
     end
 
-% disp(sum(output)) <=> xy(1)*xy(2)
+    newData = zeros(usedGraylevels, 1);
+    
+    index = 1;
+    for i=1:1:grays
+        if data(i) ~= 0
+           newData(index) = data(i);
+           index = index + 1;
+        end
+    end
 
