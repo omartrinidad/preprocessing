a4cf7c8ca113ad95d0e47d5e80333bdcb3375c71
Omar Trinidad Gutiérrez Méndez <omar.vpa@gmail.com>
2013-05-03 10:44:49 -0500

    Advance in histogram shrinking

--- shrinkHistogram.m
+++ shrinkHistogram.m
@@ -1,17 +1,18 @@
 % Shrink histogram to compress the image
 
-function [newData usedGraylevels] = shrinkHistogram(image, grayrange)
+function [newData imageCopy] = shrinkHistogram(image, grayrange)
     %image = imadjust(image, [0, 1.0], [0.5, 0.8]);
     grays = 2^grayrange;
     data = imhist(image, grays); 
-    usedGraylevels = 0;
+    %[x y] = size(data);
+    usedGrayLevels = 0;
     for i=1:1:grays
         if data(i) ~= 0
-            usedGraylevels = usedGraylevels + 1;
+            usedGrayLevels = usedGrayLevels + 1;
         end
     end
 
-    newData = zeros(usedGraylevels, 1);
+    newData = zeros(usedGrayLevels, 1);
     
     index = 1;
     for i=1:1:grays
@@ -21,3 +22,35 @@ function [newData usedGraylevels] = shrinkHistogram(image, grayrange)
         end
     end
 
+    % compresion
+    [height width] = size(image);
+    imageCopy = repmat(uint8(0), height, width);
+    divider = 0.0;
+    maxLevel = double(max(image(:)));
+    
+    while 1
+        divider = divider + 0.01;
+        if maxLevel/divider <= 255
+            break;
+        end
+    end
+    for h=1:1:height
+        for w=1:1:width
+           imageCopy(h, w) = image(h, w)/divider;
+        end
+    end
+
+    %second method
+    %[height width] = size(image);
+    %imageCopy = repmat(uint8(0), height, width);
+    %minValue = double(min(image(:)));
+    %maxValue = double(max(image(:)));
+    %piece = double(255.0/(maxValue - minValue));
+    %for h=1:1:height
+    %    for w=1:1:width
+    %       imageCopy(h, w) = uint8((image(h, w) - minValue)*piece);
+    %    end
+    %end
+    
+    %third method    
+    %imageCopy = uint8(image/256);
