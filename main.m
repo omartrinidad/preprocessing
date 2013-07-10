fad004277648f8d02d5aa085e5104fc451d8aa49
Omar Trinidad Gutiérrez Méndez <omar.vpa@gmail.com>
2013-07-10 15:51:51 -0500

    Configured Singleton instance

--- main.m
+++ main.m
@@ -4,33 +4,45 @@
 
 function varargout = main(varargin)
     % Description of GUI and main variables
-    % h is a handle structure
-    close all;
-    
-    sizeOfScreen = get(0,'ScreenSize');
-    hMainFigure = figure('position', sizeOfScreen,...
-           'name', 'Preprocessing System of Mammograms',...
-           'numbertitle', 'off', 'dockcontrols', 'off', ...
-           'menubar', 'none');
-    set(hMainFigure, 'keypressfcn', @mainKeyPressFcn);
 
-    % GUI Controls
-    % Position -> [hposition vposition hsize vsize] percentage
-    positionLeftPanel = [.02 .02 .15 .94];
-    ph = uipanel('parent', hMainFigure, 'title', 'Image selector',...
-                 'position', positionLeftPanel);
+    hMainFigure = findall(0, 'tag', 'maingui');
+    if (isempty(hMainFigure))
+        sizeOfScreen = get(0,'ScreenSize');
+        hMainFigure = figure('position', sizeOfScreen,...
+               'name', 'Preprocessing System of Mammograms',...
+               'numbertitle', 'off', 'dockcontrols', 'off', ...
+               'tag', 'maingui', 'menubar', 'none');
+        set(hMainFigure, 'keypressfcn', @mainKeyPressFcn);
 
-    % Menus
-    mainMenu = uimenu('label', 'File');
-    openFile = uimenu(mainMenu, 'label', 'Load image(s)', ...
-                      'callback', @openFileCallback);
+        % GUI Controls
+        % Position -> [hposition vposition hsize vsize] percentage
+        positionLeftPanel = [.02 .02 .15 .94];
+        ph = uipanel('parent', hMainFigure, 'title', 'Image selector',...
+                     'position', positionLeftPanel);
 
-    % Axe
-    position = [.17 .02 .81 .94];
-    [width height] = getSizeOfAxe(position, sizeOfScreen);
-    mainAxe = axes('parent', hMainFigure, 'visible', 'on', ...
-                   'position', position);
-    set(mainAxe, 'yticklabel', '', 'xticklabel', '');
+        % Menus
+        mainMenu = uimenu('label', 'File');
+        openFile = uimenu(mainMenu, 'label', 'Load image(s)', ...
+                          'callback', @openFileCallback);
+
+        % Axe
+        position = [.17 .02 .81 .94];
+        [width height] = getSizeOfAxe(position, sizeOfScreen);
+        mainAxe = axes('parent', hMainFigure, 'visible', 'on', ...
+                       'position', position);
+        set(mainAxe, 'yticklabel', '', 'xticklabel', '');
+
+        % variables visibles in the inner functions
+        I = [];
+        sizeOfStep = 35;
+        a = 1;
+        b = 1;
+        section = [];
+    else
+        figure(hMainFigure);
+    end %end of Singleton
+
+    % ------------- Functions and callbacks
 
     % function to convert percentage to pixels 
     function [width height] = getSizeOfAxe(percentages, sizeOfScreen)
@@ -55,18 +67,18 @@ function varargout = main(varargin)
         end
         % load and show the image in the main Axe
         pathfile = strcat(path, files{1});
-        I = dicomread(pathfile);
-        I = reduceWorkArea(I);
-        [h w] = size(I);
-        I = f12to16bits(I);
-        % I = adpmedian(I, 7);
-        if height < h && width < w
-            imshow(I(1:height, 1:width));
-        else
-            imshow(I(1:h, 1:w));
-        end
-        colormap bone;
+        fileSelected = dicomread(pathfile);
+        showImage(fileSelected);
         % load and show the images(s) in the left side
+        showImagesInPanel(path, files);
+    end % ending openFileCallback function
+
+    function showImageSelected(hObject, eventdata, fileSelected)
+        axes(mainAxe); % use the Axe selected
+        showImage(fileSelected);
+    end % ending selectImage function
+
+    function showImagesInPanel(path, files)
         % calculate left and right margins
         space = 0.01;
         left = positionLeftPanel(1) + space;
@@ -85,26 +97,30 @@ function varargout = main(varargin)
             up = down + heightOfImage;
             hAxes(i) = axes('position', [left down right-left up-down]);
             down = up + space;
-            imagesc(fileToSelect, 'buttondownfcn', {@showImageSelected, fileToSelect});
+            imagesc(fileToSelect, 'buttondownfcn', ...
+                    {@showImageSelected, fileToSelect});
             axis off; axis image;
         end
-    end % ending openFileCallback function
-
-    function showImageSelected(hObject, eventdata, fileSelected)
+    end
+ 
+    function showImage(fileSelected)
         axes(mainAxe); % use the Axe selected
         I = reduceWorkArea(fileSelected);
         [h w] = size(I);
         I = f12to16bits(I);
         % I = adpmedian(I, 7);
         if height < h && width < w
-            imshow(I(1:height, 1:width));
+            section = I(1:height, 1:width);
+            imshow(section);
         else
-            imshow(I(1:h, 1:w));
+            section = I(1:h, 1:w);
+            imshow(section);
         end
         colormap bone;
     end % ending selectImage function
     
-    % function to catch keyboard events
+    % function to catch keyboard events, is possible control the 
+    % image with the movement keys
     function mainKeyPressFcn(hObject, eventdata)
         if strcmp(eventdata.Key, '0')
             disp('más');
@@ -113,11 +129,16 @@ function varargout = main(varargin)
         elseif strcmp(eventdata.Key, 'uparrow')
             disp('uparrow');
         elseif strcmp(eventdata.Key, 'downarrow')
-            disp('downarrow');
+            a = a + sizeOfStep;
+            height = height + sizeOfStep;
+            section = I(a:height, 1:width);
         elseif strcmp(eventdata.Key, 'leftarrow')
             disp('leftarrow');
         elseif strcmp(eventdata.Key, 'rightarrow')
             disp('rightarrow');
         end
+        axes(mainAxe);
+        imshow(section);
+        colormap bone;
     end
 end % ending main function
