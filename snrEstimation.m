c9b05438b08b48a51756b7f169a89cf1ed890491
omartrinidad <omar.vpa@gmail.com>
2013-09-22 19:37:25 -0500

    Deleted snrEstimation

--- snrEstimation.m
+++
@@ -1,8 +0,0 @@
-% signal noise to ratio estimation
-
-function snr = snrEstimation(signalnoise, signal) 
-    noise = signalnoise - signal;
-    figure, imshow(noise);
-    signalmean = mean(signal(:));
-    stdnoise = std2(noise(:));
-    snr = signalmean/stdnoise;
